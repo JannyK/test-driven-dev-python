51416895ed73f36a780c1edcde57926b48317adc
Janny Kinende <janny@haiinteraktiv.no>
2014-11-07 12:05:48 +0100

    Front page HTML now generated from template

--- lists/tests.py
+++ lists/tests.py
@@ -15,6 +15,9 @@ class HomePageTest(TestCase):
 		response = home_page(request)
 		expected_html = render_to_string('home.html')
 		self.assertEqual(response.content.decode(), expected_html)
+		#decode() convert bytes into python unicode string, which allows us to
+		#compare strings with strings
+		
 
 		
 
